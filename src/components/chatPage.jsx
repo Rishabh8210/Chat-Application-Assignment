5059c0a4d695bc2d767c11084a6cbb658a8c4b72
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-07-20 14:43:04 +0530

    All features done,, final commit

--- src/components/chatPage.jsx
+++ src/components/chatPage.jsx
@@ -13,31 +13,33 @@ import axios from 'axios'
 import { decodeJWT } from '../utils/helper'
 import { io } from 'socket.io-client'
 const ChatPage = () => {
-  const {id} = useParams()
+  const { id } = useParams()
   const [chatMessages, setChatMessages] = useState([]);
   const [user, setUser] = useState('');
+  const [onlineUsers, setOnlineUsers] = useState([]);
   const [message, setMessage] = useState('')
   // const token = localStorage.getItem('token');
   const token = localStorage.getItem('token')
-  
-  
+
+
   const [userId, setUserId] = useState(null)
-  const [isNavOpen, setNavOpen] = useState(false);
+  const [isOnline, setIsOnline] = useState(false);
   const [socket, setSocket] = useState(null)
-  
+
   useEffect(() => {
     setSocket(io('http://localhost:8000'))
   }, [])
-  
+
   useEffect(() => {
     const token = localStorage.getItem("token");
-    if(token){
+    if (token) {
       const response = decodeJWT(token)
       setUserId(response?.userId)
       console.log(response?.userId)
       socket?.emit("addUser", response?.userId);
       socket?.on("getUsers", (users) => {
         console.log("Online Users:", users);
+        setOnlineUsers(users)
       });
       socket?.on('getMessage', (data) => {
         console.log("Receiverd data", data);
@@ -46,8 +48,22 @@ const ChatPage = () => {
     }
   }, [socket]);
   useEffect(() => {
-    async function getDetails(){
-      try{
+    function checkOnline(){
+      if(onlineUsers){
+        const filter = onlineUsers.filter((online) => online?.userId == id)
+        if(filter.length > 0)
+          setIsOnline(true);
+        else{
+          setIsOnline(false);
+        }
+      }
+    }
+    checkOnline();
+  }, [onlineUsers])
+  useEffect(() => {
+    
+    async function getDetails() {
+      try {
         const currUser = await axios.get(`http://localhost:3001/api/v1/users/${id}`, {
           headers: {
             "Authorization": `Bearer ${token}`
@@ -55,15 +71,16 @@ const ChatPage = () => {
         })
         // console.log(currUser?.data?.data)
         setUser(currUser?.data?.data)
-      }catch(err){
+      } catch (err) {
         console.log(err)
       }
-    } 
+    }
     getDetails();
   }, [id])
-  
+
   useEffect(() => {
-    async function fetchMessages(){
+    
+    async function fetchMessages() {
       try {
         const fetchedMessages = await axios.get(`http://localhost:3001/api/v1/conversations/${id}`, {
           headers: {
@@ -80,12 +97,12 @@ const ChatPage = () => {
   }, [])
 
   // const dispatch = useDispatch()
-  function handleChange(e){
+  function handleChange(e) {
     setMessage(e.target.value);
   }
-  async function handleClick(){
+  async function handleClick() {
 
-    if(message.length > 0){
+    if (message.length > 0) {
       console.log(userId, id, message)
       socket?.emit('sendMessage', {
         senderId: userId,
@@ -97,7 +114,7 @@ const ChatPage = () => {
         message: message
       }
       try {
-        const saveMessage = await axios.post(`http://localhost:3001/api/v1/conversations`,data, {
+        const saveMessage = await axios.post(`http://localhost:3001/api/v1/conversations`, data, {
           headers: {
             "Authorization": `Bearer ${token}`
           }
@@ -110,32 +127,35 @@ const ChatPage = () => {
       setMessage('');
     }
   }
-  if(!token) {
+  if (!token) {
     return window.location = '/'
   }
   return (
     <div className='h-screen w-screen'>
-        <div className='h-[89] w-full p-3 flex gap-5 items-center border-b-2 border-black'>
-            <Link to={'/chats'}><img className='w-7' src={back} alt='Go back' /></Link>
-            <div className='flex h-fit w-full items-center gap-3'>
-                <img className='w-10 rounded-full' src={userImg} alt='User Logo'/>
-                <p className='text-xl'>{user?.name}</p>
-            </div>
+      <div className='h-[89] w-full p-3 flex gap-5 items-center border-b-2 border-black'>
+        <Link to={'/chats'}><img className='w-7' src={back} alt='Go back' /></Link>
+        <div className='flex h-fit w-full items-center gap-3'>
+          <img className='w-10 rounded-full' src={userImg} alt='User Logo' />
+          <p className='text-xl'>{user?.name}</p>
+          {
+            isOnline ? <p className='font-semibold'>Online</p> : <p className='font-semibold'>Offline</p>
+          }
         </div>
-        <div className='h-[82%] py-2 px-3 w-full overflow-scroll flex flex-col flex-shrink-0 gap-2'>
-            {
-                chatMessages && chatMessages.map((message, index) => {
-                    if(message?.receiverId != id)
-                       return <ReveiverMessageCard key={message?.id} message={message} />
-                    else
-                      return <SenderMessageCard key={message?.id} message = {message}/>
-                })
-            }            
-        </div>  
-        <div className='fixed bottom-0 left-0 h-[10%] bg-white w-full flex items-center justify-between md:justify-center md:gap-10 px-3'>
-            <input className='h-11 w-[90%] text-lg px-4 outline-none transition-all rounded-full border-2 bg-gray-300' type='text' name='search' placeholder='type message...' value={message} onChange={(e) => handleChange(e)}/>
-            <img className='w-7' src={send} alt='send' onClick={() => handleClick()}/>
-        </div>
+      </div>
+      <div className='h-[82%] py-2 px-3 w-full overflow-scroll flex flex-col flex-shrink-0 gap-2'>
+        {
+          chatMessages && chatMessages.map((message, index) => {
+            if (message?.receiverId != id)
+              return <ReveiverMessageCard key={message?.id} message={message} />
+            else
+              return <SenderMessageCard key={message?.id} message={message} />
+          })
+        }
+      </div>
+      <div className='fixed bottom-0 left-0 h-[10%] bg-white w-full flex items-center justify-between md:justify-center md:gap-10 px-3'>
+        <input className='h-11 w-[90%] text-lg px-4 outline-none transition-all rounded-full border-2 bg-gray-300' type='text' name='search' placeholder='type message...' value={message} onChange={(e) => handleChange(e)} />
+        <img className='w-7' src={send} alt='send' onClick={() => handleClick()} />
+      </div>
     </div>
   )
 }
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -14,3 +14,18 @@ export const decodeJWT = (token) => {
     }
 
 }
+
+export const filterOnlineUser = (filteredUser, onlineUsers) => {
+    // Create a map of userId to status from the onlineUsers array
+    console.log(filteredUser, onlineUsers)
+    const statusMap = onlineUsers.reduce((map, { userId, status }) => {
+        map[userId] = status;
+        return map;
+    }, {});
+
+    // Add the status to each user in filteredUser based on userId
+    return filteredUser.map(user => ({
+        ...user,
+        status: statusMap[user.id] || 'Offline' // Default to 'Offline' if no status is found
+    }));
+};
